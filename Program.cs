7b70c528e0939cb0b2c901b9dc7d25699e1f2a56
Bartosz Mosica <b.mosica@pricely.pl>
2024-04-18 18:06:55 +0200

    Added OpenTelemetry and logger

--- Program.cs
+++ Program.cs
@@ -1,12 +1,27 @@
 using BinaryTreeService.Model;
 using BinaryTreeService.Services;
 using Microsoft.AspNetCore.Mvc;
+using OpenTelemetry.Logs;
+using OpenTelemetry.Metrics;
+using OpenTelemetry.Resources;
+using OpenTelemetry.Trace;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddScoped<IBinaryTreeProcessor, BinaryTreeProcessor>();
+
+builder.Logging.AddOpenTelemetry(options =>
+{
+    options.SetResourceBuilder(ResourceBuilder.CreateDefault()
+    .AddService(builder.Environment.ApplicationName))
+    .AddConsoleExporter();
+});
+builder.Services.AddOpenTelemetry()
+.ConfigureResource((resourceBuilder) => resourceBuilder.AddService(builder.Environment.ApplicationName))
+.WithTracing(tracing => tracing.AddAspNetCoreInstrumentation().AddConsoleExporter())
+.WithMetrics(metrics => metrics.AddAspNetCoreInstrumentation().AddConsoleExporter());
 
 var app = builder.Build();
 
@@ -16,15 +31,19 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
-app.MapPost("/mirror-binarytree", ([FromBody] BinaryTree binaryTree, IBinaryTreeProcessor processor) =>
+app.MapPost("/mirror-binarytree", ([FromBody] BinaryTree binaryTree, IBinaryTreeProcessor processor, ILogger<Program> logger) =>
 {
+    logger.LogInformation("MirrorBinaryTree method is called");
+
     if (binaryTree == null)
     {
+        logger.LogError("Binary tree is required");
         return Results.BadRequest("Binary tree is required");
     }
 
     if (!binaryTree.IsValid())
     {
+        logger.LogError("Binary tree structure is improper");
         return Results.BadRequest("Binary tree structure is improper");
     }
 
